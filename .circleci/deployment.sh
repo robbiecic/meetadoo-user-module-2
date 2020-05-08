be4209f886e1da511a0b9a6786933414e4ee454b
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-08 20:31:38 +0100

    gcloud service key json still causing issues

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -1,6 +1,9 @@
 # !/bin/bash
 docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
-echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account circleci@meetadoo.iam.gserviceaccount.com --key-file=-
+# Store service account
+echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json
+# Initialize gcloud CLI
+gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
-gcloud docker -- push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1
+gcloud docker --push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1
