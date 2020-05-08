fb5258c0d8e273c47639e66fe636065ea01daad5
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-08 20:21:40 +0100

    Playing around with gcloud commands

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -1,7 +1,6 @@
 # !/bin/bash
-echo ${GCLOUD_SERVICE_KEY} > ${HOME}/gcp-key.json
 docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
-cat ${HOME}/gcp-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcp-key.json
+echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
+gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 gcloud docker -- push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1
