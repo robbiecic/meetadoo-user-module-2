a4978cb7a3826f2e9a08782af8027350348a6e2d
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-08 19:57:14 +0100

    Adding debug info to help

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 echo ${GCLOUD_SERVICE_KEY} > ${HOME}/gcp-key.json
 docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
-echo ${HOME}/gcp-key.json
+cat ${HOME}/gcp-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcp-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud docker -- push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1
