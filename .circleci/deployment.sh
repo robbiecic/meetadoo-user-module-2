4a5800773c8f01ef94a9520a2b801a56fe1fb8ad
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-08 20:37:57 +0100

    Adding more debug info

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -2,6 +2,7 @@
 docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
 # Store service account
 echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json
+cat ${HOME}/gcloud-service-key.json
 # Initialize gcloud CLI
 gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
