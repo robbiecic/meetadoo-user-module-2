5788e183b07246358ef47da611d7421466fa7452
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-08 20:08:28 +0100

    gcloud key json file format screwed

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
-echo ${GCLOUD_SERVICE_KEY} > ${HOME}/gcp-key.json
+echo ${GCLOUD_SERVICE_KEY} > base64 --decode --ignore-garbage > ${HOME}/gcp-key.json
 docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
 cat ${HOME}/gcp-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcp-key.json
