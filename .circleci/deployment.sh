703d62ea70ccd9ceb876e011ad400bc26aaac35a
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-08 21:03:41 +0100

    Just trying to focus on gcloud now

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -1,7 +1,9 @@
 # !/bin/bash
-docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
+# docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
 echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json
+cat ${HOME}/gcloud-service-key.json
+echo "Attempting to authenticated to gcloud ... "
 gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
-gcloud docker push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1
+# gcloud docker push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1
