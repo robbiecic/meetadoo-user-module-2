33ea48a8c4ef4d2c14ef4ab219e9ad2db183ecaf
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-08 20:46:22 +0100

    Seem to regress... Trying to add back debug info

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
 echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
+cat ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
