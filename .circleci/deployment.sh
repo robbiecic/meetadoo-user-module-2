5063951a15ea487bbe9ae2b9306a11e5ca17e20f
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-08 20:27:10 +0100

    Added account as was required by google sdk for some reason... Might need to revisit this

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
-echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
+echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account circleci@meetadoo.iam.gserviceaccount.com --key-file=-
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 gcloud docker -- push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1
