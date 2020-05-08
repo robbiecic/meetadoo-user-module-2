5dee753fd87a842429dac7ca935727e556037806
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-08 20:54:27 +0100

    Removed base 64 encoded crap

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
-echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
+echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
