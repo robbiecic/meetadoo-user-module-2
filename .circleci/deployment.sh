13fbb3626e4dc5d872407bb57c92480dae22a8e0
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-09 11:00:40 +0100

    dsgsdg

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -2,7 +2,7 @@
 # docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 .
 echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key1.json
 cat ${HOME}/gcloud-service-key1.json
-sed $'s/[^[:print:]\t]//g' ${HOME}/gcloud-service-key1.json > ${HOME}/gcloud-service-key.json
+sed -e 's/[;,()'\'']/ /g;s/  */ /g' ${HOME}/gcloud-service-key1.json > ${HOME}/gcloud-service-key.json
 cat ${HOME}/gcloud-service-key.json
 echo "\n Attempting to authenticated to gcloud ... \n"
 gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
