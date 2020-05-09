de337f0cdc9285fb34d101d25b8b450dc026810b
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-09 15:31:25 +0100

    Trying to avoid multiple docker images being pushed, so tagging build version after using gcloud add-tag

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -2,15 +2,9 @@
 # Set -e ensures errors aren't surpressed and traverse to circleci caller
 set -e
 
-# Create an intuitive tag with the current date appending with the CirclCi build number
-echo "Creating build version number..."
-DATE=$(date +'%Y.%m.%d')
-VERSION=$DATE.$CIRCLE_BUILD_NUM
-echo "This build version is " $VERSION "\n"
-
 # Build and tag docker image
 echo "Building docker image... \n"
-docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$VERSION .
+docker build --rm=false -t gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 -t .
 
 # Pull gcloud credentials from environmental variable
 echo "Exporting environment variable to json file... \n"
@@ -24,6 +18,15 @@ gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 
-# Finally, push docker to gcloud container registry
+# Push docker to gcloud container registry tagged by the Git Hash value
 echo "Pushing docker image to Google Cloud Container Registry... \n"
-gcloud docker -- push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}
+gcloud docker -- push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1
+
+# Create an intuitive tag with the current date appending with the CirclCi build number
+echo "Creating build version number..."
+DATE=$(date +'%Y.%m.%d')
+VERSION=$DATE.$CIRCLE_BUILD_NUM
+echo "This build version is " $VERSION "\n"
+
+# Tag image with build versoin
+gcloud container images add-tag gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1 gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$VERSION
