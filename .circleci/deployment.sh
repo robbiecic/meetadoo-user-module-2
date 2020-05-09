2204d8da275a897edd1ab229aff525c49dcde0d6
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-09 17:19:32 +0100

    Normalising scripts

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Connec to gcloud
-.circleci/gcloud.sh
+sh .circleci/gcloud.sh
 
 # Push docker to gcloud container registry tagged by the Git Hash value
 echo "Ready to deploy to GKE ... "
