99420620046cd4aa23e392d554efdf37f3af5906
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-09 11:21:15 +0100

    Docker push aint working

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -10,4 +10,4 @@ gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 echo "Pushing docker image to Google Cloud Container Registry... \n"
-gcloud docker push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1
+gcloud docker -- push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:$CIRCLE_SHA1
