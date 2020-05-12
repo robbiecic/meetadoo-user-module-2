46804c94b2805a2c5637c79dd417a18f2509000b
RobertCicero <robert.cicer@iress.com>
2020-05-12 08:32:34 +0100

    Attempting to connect to GKE

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -7,5 +7,12 @@ sh .circleci/gcloud.sh
 
 # Push docker to gcloud container registry tagged by the Git Hash value
 echo "Ready to deploy to GKE ... "
+
+# Generate kubeconfig to access cluster
+gcloud container clusters get-credentials user-api-cluster
+
+# Check current context
+kubectl config current-context
+
 # kubectl create deployment hello-web --image=gcr.io/${PROJECT_ID}/hello-app:v1
 # kubectl expose deployment hello-web --type=LoadBalancer --port 80 --target-port 8080
