69cb4c491835db30cd8a4140eebead879bb4338e
Robert Cicero <robert.cicero.rc@gmail.com>
2020-05-23 13:46:11 +0100

    Changed instance name to environmental variable and add tags to allow http traffic

--- .circleci/deployment.sh
+++ .circleci/deployment.sh
@@ -9,15 +9,20 @@ sh .circleci/gcloud.sh
 echo "Ready to deploy to GCP ... "
 
 # Create compute instance from the image correspdonding to this build
-# gcloud compute instances create-with-container meetadoo-user-api-instance \
+# gcloud compute instances create-with-container ${GOOGLE_INSTANCE_NAME} \
 #      --container-image 'gcr.io/meetadoo/'"${IMAGE_NAME}"':'"$CIRCLE_SHA1"'"'
 
 # Once instance has been created, when need to run update instead
-gcloud compute instances update-container meetadoo-user-api-instance \
+gcloud compute instances update-container ${GOOGLE_INSTANCE_NAME} \
     --container-image 'gcr.io/meetadoo/'"${IMAGE_NAME}"':'"$CIRCLE_SHA1"'"'
 
+# Allow http/s traffic
+ gcloud compute instances add-tags ${GOOGLE_INSTANCE_NAME} \
+    --tags http-server,https-server
+
+
 # To change machine type, run these commands
-# gcloud compute instances stop meetadoo-user-api-instance
-# gcloud compute instances set-machine-type meetadoo-user-api-instance \
+# gcloud compute instances stop ${GOOGLE_INSTANCE_NAME}
+# gcloud compute instances set-machine-type ${GOOGLE_INSTANCE_NAME} \
 #     --zone ${GOOGLE_COMPUTE_ZONE} --machine-type f1-micro
-# gcloud compute instances start meetadoo-user-api-instance
+# gcloud compute instances start ${GOOGLE_INSTANCE_NAME}
